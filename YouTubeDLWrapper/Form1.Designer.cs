0f5214972c18f6abfdd836bc8fd0be08149135e6
Tomachi[Main] <lego@aol.jp>
2019-01-25 17:37:52 +0900

    first commit

--- YouTubeDLWrapper/Form1.Designer.cs
+++ YouTubeDLWrapper/Form1.Designer.cs
@@ -28,13 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Artist = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 327);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(800, 124);
+            this.textBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 302);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(800, 29);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Title,
+            this.Artist,
+            this.URL,
+            this.Status});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 211);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Title
+            // 
+            this.Title.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Title.HeaderText = "Title";
+            this.Title.MinimumWidth = 10;
+            this.Title.Name = "Title";
+            this.Title.ReadOnly = true;
+            // 
+            // Artist
+            // 
+            this.Artist.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Artist.HeaderText = "Artist";
+            this.Artist.Name = "Artist";
+            this.Artist.ReadOnly = true;
+            // 
+            // URL
+            // 
+            this.URL.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.URL.HeaderText = "URL";
+            this.URL.Name = "URL";
+            this.URL.ReadOnly = true;
+            // 
+            // Status
+            // 
+            this.Status.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.textBox2.Location = new System.Drawing.Point(12, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(561, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "https://www.youtube.com/watch?v=";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(579, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("ＭＳ Ｐゴシック", 12F);
+            this.button2.Location = new System.Drawing.Point(636, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Download Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("ＭＳ Ｐゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.Location = new System.Drawing.Point(0, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 38);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "ALL Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(711, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 32);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "YouTubeDLWrapper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Artist;
+        private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- YouTubeDLWrapper/Form1.cs
+++ YouTubeDLWrapper/Form1.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 
 namespace YouTubeDLWrapper
@@ -15,6 +20,164 @@ namespace YouTubeDLWrapper
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            addLogBox("Application Start");
+            
+            if (!File.Exists("youtube-dl.exe"))
+            {
+                MessageBox.Show("同じディレクトリ内にyoutube-dl.exeを配置してください。",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            if (!Directory.Exists("output"))
+            {
+                Directory.CreateDirectory("output");
+            }
+        }
+        void addLogBox(string text)
+        {
+            string date = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+            textBox1.Text = textBox1.Text + "[" + date + "] " + text + Environment.NewLine;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox2.Text;
+            addLogBox("Add Process Start: " + url);
+            string pattern = "[/?=]?([-\\w]{11})";
+            Match match = Regex.Match(url, pattern);
+            if (!match.Success)
+            {
+                addLogBox("Pattern mismatch [debug: 1]");
+                MessageBox.Show("YouTubeのIDをURLから取得できませんでした。(1)",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!match.Groups[1].Success)
+            {
+                addLogBox("Pattern mismatch [debug: 2]");
+                MessageBox.Show("YouTubeのIDをURLから取得できませんでした。(2)",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+            string vid = match.Groups[1].Value;
+            addLogBox("VID: " + vid);
+            string res = null;
+            try
+            {
+                res = new WebClient().DownloadString("http://youtube.com/get_video_info?video_id=" + vid);
+            }catch(WebException ex)
+            {
+                addLogBox("YouTubeAPI Response: " + ex.Message);
+                MessageBox.Show("指定されたYouTubeURLの動画情報を取得できませんでした。(1)" + Environment.NewLine +
+                    ex.Message,
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+            string title = GetArgs(res, "title", '&');
+            addLogBox("MovTitle: " + title);
+            if (title.Equals(string.Empty))
+            {
+                MessageBox.Show("指定されたYouTubeURLの動画情報を取得できませんでした。(2)",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+            string author = GetArgs(res, "author", '&');
+            addLogBox("MovAuthor: " + author);
+
+            foreach (DataGridViewRow data in dataGridView1.Rows.Cast<DataGridViewRow>())
+            {
+                if(vid.Equals(data.Cells[2].Value.ToString()))
+                {
+                    addLogBox("MovAuthor: " + author);
+                    MessageBox.Show("指定された動画はすでにキューに登録されています。",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            dataGridView1.Rows.Add(title, author, vid, "待機中");
+            addLogBox("VID: " + vid + " Added!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            addLogBox("DL Process Start");
+
+            addLogBox("Files Count: " + dataGridView1.Rows.Count);
+
+            int success = 0;
+            int error = 0;
+
+            progressBar1.Maximum = dataGridView1.Rows.Count;
+            progressBar1.Value = 0;
+
+            foreach (DataGridViewRow data in dataGridView1.Rows.Cast<DataGridViewRow>())
+            {
+                string vid = data.Cells[2].Value.ToString();
+                addLogBox("DL " + vid + " Start...");
+                Process p = Process.Start(
+                    "youtube-dl.exe",
+                    "-c --ignore-config https://www.youtube.com/watch?v=" + vid +
+                    " -o output\\%(title)s.%(ext)s");
+
+                p.WaitForExit();
+                if (p.ExitCode == 0)
+                {
+                    addLogBox("Success!");
+                    data.Cells[3].Value = "成功";
+                    success++;
+                }
+                else
+                {
+                    addLogBox("Error...");
+                    data.Cells[3].Value = "エラー";
+                    error++;
+                }
+                progressBar1.Value++;
+                
+                addLogBox("DL " + vid + " End...");
+            }
+            MessageBox.Show("処理終了" + Environment.NewLine + "成功数: " + success + Environment.NewLine + "失敗数: " + error,
+                    "Result",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            addLogBox("成功数: " + success);
+            addLogBox("失敗数: " + error);
+            button2.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            addLogBox("Cleared List");
+        }
+
+        // https://stackoverflow.com/questions/22108268/how-to-get-youtube-video-title-using-url
+        private static string GetArgs(string args, string key, char query)
+        {
+            var iqs = args.IndexOf(query);
+            return iqs == -1
+                ? string.Empty
+                : HttpUtility.ParseQueryString(iqs < args.Length - 1
+                    ? args.Substring(iqs + 1) : string.Empty)[key];
         }
     }
 }
